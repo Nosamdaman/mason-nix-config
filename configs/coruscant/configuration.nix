c6e95fc4d73266215c30bc2fc31c85347ec7ebc9
Mason Davy <masonnosam11@gmail.com>
2026-05-25 09:35:16 -0400

    Setting the text mode option
    This prevents certain messages from being logged once we get to the
    logon screen.

--- configs/coruscant/configuration.nix
+++ configs/coruscant/configuration.nix
@@ -45,6 +45,7 @@
     services = {
         greetd = {
             enable = true;
+            useTextGreeter = true;
             settings = {
                 default_session = {
                     command = "${pkgs.tuigreet}/bin/tuigreet --time --remember-session";
